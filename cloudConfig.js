fe055ec31a31c69be2e3c30ce9bd322f0ebbf21f
Pankhudi roy <pankhudiroy2@gmail.com>
2026-08-18 15:52:29 +0530

    Icon change and Review improvement

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,22 +1,39 @@
+// const cloudinary = require('cloudinary').v2;
+// const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+// cloudinary.config({
+//     cloud_name : process.env.CLOUD_NAME,
+//     api_key : process.env.CLOUD_API_KEY,
+//     api_secret : process.env.CLOUD_API_SECTRET
+
+// });
+
+// const storage = new CloudinaryStorage({
+//   cloudinary: cloudinary,
+//   params: {
+//     folder: 'Stayzy',
+//     allowed_formats: ["jpeg", "png", "jpeg"],
+//   },
+// });
+
+// module.exports ={
+//     cloudinary,
+//     storage,
+// };
+
 const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const CloudinaryStorage = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name : process.env.CLOUD_NAME,
-    api_key : process.env.CLOUD_API_KEY,
-    api_secret : process.env.CLOUD_API_SECTRET
-
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECTRET
 });
 
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'Stayzy',
-    allowedFormat: ["jpeg", "png", "jpeg"],
-  },
+  cloudinary,
+  folder: 'Stayzy',
+  allowedFormats: ['jpeg', 'png', 'jpg']
 });
 
-module.exports ={
-    cloudinary,
-    storage,
-};
+module.exports = { cloudinary, storage };
